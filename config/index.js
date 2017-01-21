284799c5648122c8d2dd5a2cb1424449e7175cff
Chia-liang Kao <clkao@clkao.org>
2017-01-21 12:34:45 +0800

    adjust test import so test config is not required

--- config/index.js
+++ config/index.js
@@ -4,7 +4,5 @@
  * is set as 'test'. Otherwise it uses the default config file
  */
 
-import config from './config.json';
-import testConfig from './config.test.json';
-
-export default process.env.NODE_ENV === 'test' ? testConfig : config;
+export default process.env.NODE_ENV === 'test'
+  ? require('./config.test.json') : require('./config.json');
