cfc13595b25c129de8cfe2bfc159f27817695861
Ronny Wang <ronnywang@gmail.com>
2018-05-18 15:49:48 +0800

    move clean service worker code to index

--- client/app/index.js
+++ client/app/index.js
@@ -2,6 +2,3 @@ jQuery(function() {
   require('./Initializer')();
   window.hackdash.startApp = require('./HackdashApp');
 });
-
-// clean /power/* service worker
-window.navigator.serviceWorker.getRegistrations().then(function(regs){for(var i=0;i<regs.length;i++){regs[i].unregister();}});
