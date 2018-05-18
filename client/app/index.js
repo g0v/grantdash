bc2f22342a2a06e2d71abf39136c48fd2b42932f
Ronny Wang <ronnywang@gmail.com>
2018-05-18 15:37:56 +0800

    add var before variable

--- client/app/index.js
+++ client/app/index.js
@@ -4,4 +4,4 @@ jQuery(function() {
 });
 
 // clean /power/* service worker
-navigator.serviceWorker.getRegistrations().then(function(regs){for(i=0;i<regs.length;i++){regs[i].unregister();}});
+navigator.serviceWorker.getRegistrations().then(function(regs){for(var i=0;i<regs.length;i++){regs[i].unregister();}});
